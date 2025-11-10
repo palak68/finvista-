e343bbe384feb9735687e8d5520da30ece5f5ffe
Palak Sharma <siyamukeshsharma@gmail.com>
2025-11-11 00:56:38 +0530

    new feature added

--- src/landing page/home/Awards.js
+++ src/landing page/home/Awards.js
@@ -1,6 +1,45 @@
 import React from 'react';
+
 function Awards() {
-    return ( <h1>Awards Page</h1> );
+  return ( 
+    <div className="container mt-5">
+      <div className="row align-items-center">
+        {/* Image Section */}
+        <div className="col-lg-6 col-sm-12 order-lg-1 order-2 text-center">
+          <img 
+            src="media/investment.jpg" 
+            alt="Award-image" 
+            className="img-fluid rounded shadow-sm"
+          />
+        </div>
+
+        {/* Text Section */}
+        <div className="col-lg-6 col-sm-12 order-lg-2 order-1">
+          <h1>Your Financial Compass. Smart Investing</h1>
+          <p>
+            50,000+ smart investors trust <b>FinVista</b> for low-cost, high-insight trading.
+            Trade smarter, not harder.
+          </p>
+          <div className="row">
+            <div className="col-lg-6 col-sm-12">
+              <ul>
+                <li>Futures and Options</li>
+                <li>Equities & F&O</li>
+                <li>Mutual Funds</li>
+              </ul>
+            </div>
+            <div className="col-lg-6 col-sm-12">
+              <ul>
+                <li>IPOs & Bonds</li>
+                <li>FinVista Funds </li>
+                <li>Smart SIPs</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- src/landing page/home/Hero.js
+++ src/landing page/home/Hero.js
@@ -1,7 +1,22 @@
 import React from 'react';
 function Hero() {
     return ( 
-   <h1>Hero Section</h1>
+   <div className="container p-5 mb-5">
+<div className="row text-center">
+    <img src='media/homeHero.jpg' alt='Hero-image' className="mb-5" />
+    <h1 className="mt-5">
+  invest in everything
+    </h1>
+    <p>
+     online platform to invest in stocks , derivatives , mutual funds, and more..   
+    </p>
+    <button className="btn " style={ { width: '200px', height: '40px', backgroundColor: '#007bff', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',
+        margin: 'auto'
+    }}>
+  Signup Now
+    </button>
+</div>
+   </div>
     )
 }
 
