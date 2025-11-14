d4ddaaa278861808394141879bb556531292b8bf
Palak Sharma <siyamukeshsharma@gmail.com>
2025-11-15 00:04:07 +0530

    add animation in education page

--- src/landing page/home/Awards.js
+++ src/landing page/home/Awards.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import animationData from "../../assets/Isometric.json";
 
 function Awards() {
   return ( 
     <div className="container mt-5">
       <div className="row align-items-center">
-        {/* Image Section */}
+
+        {/* Animation Section */}
         <div className="col-lg-6 col-sm-12 order-lg-1 order-2 text-center">
-          <img 
-            src="media/investment.jpg" 
-            alt="Award-image" 
-            className="img-fluid rounded shadow-sm"
-          />
+          <div
+            style={{
+              width: "350px",
+              margin: "auto"
+            }}
+          >
+            <Lottie animationData={animationData} loop={true} autoplay={true} />
+          </div>
         </div>
 
         {/* Text Section */}
@@ -20,28 +26,36 @@ function Awards() {
             50,000+ smart investors trust <b>FinVista</b> for low-cost, high-insight trading.
             Trade smarter, not harder.
           </p>
+
           <div className="row">
             <div className="col-lg-6 col-sm-12">
               <ul>
                 <li>Futures and Options</li>
-                <br></br>
+                <br />
                 <li>Equities & F&O</li>
-                <br></br>
+                <br />
                 <li>Mutual Funds</li>
               </ul>
             </div>
+
             <div className="col-lg-6 col-sm-12">
               <ul>
                 <li>IPOs & Bonds</li>
-                <br></br>
-                <li>FinVista Funds </li>
-                <br></br>
+                <br />
+                <li>FinVista Funds</li>
+                <br />
                 <li>Smart SIPs</li>
               </ul>
             </div>
           </div>
-          <img src="media/pressLogos.png" alt="Press Logos" className="img-fluid mt-3" />
+
+          <img 
+            src="media/pressLogos.png" 
+            alt="Press Logos" 
+            className="img-fluid mt-3"
+          />
         </div>
+
       </div>
     </div>
   );
--- src/landing page/home/Education.js
+++ src/landing page/home/Education.js
@@ -2,9 +2,15 @@ import React from 'react';
 function Education() {
     return ( <div className='container mt-5 mb-4'>
         <div className='row'>
-            <div className='col-lg-6 col-sm-12 p-5'>
-
-            </div>
+            <div className='col-lg-6 col-sm-12 p-5 text-center'>
+  <iframe 
+    src="https://my.spline.design/untitled-63be99ed490b03e6219e21f88cc37970/" 
+    frameBorder="0" 
+    width="100%" 
+    height="500px"
+    style={{ border: "none", borderRadius: "20px" }}
+  ></iframe>
+</div>
             <div className='col-lg-6 col-sm-12 p-5'>
                 <h1>Free and open market Education</h1>
                 <p className='mt-4 '> Finvista Academy , the largest online stock market education library covering everything from basic to advanced trading strategies</p>
